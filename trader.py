ccee94a49e9dcd3217289464168c5cc99e911fcd
wizche <sergio.paganoni@gmail.com>
2021-02-17 21:33:43 +0100

    Removed rich, improved code

--- trader.py
+++ trader.py
@@ -2,23 +2,15 @@
 import json
 import sys
 import itertools
-from datetime import datetime
 
-from rich import box, print
-from rich.align import Align
-from rich.console import Console, RenderGroup
-from rich.layout import Layout
-from rich.panel import Panel
-from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
-from rich.syntax import Syntax
-from rich.table import Table
-from rich.text import Text
 from trading import TradingEngine, Strategy
 
-# this account is the one using to transfer founds
+# days between buy
 trading_interval_days = 7
+# periods (for simulation)
+trading_periods = 30
 # how many products should considerate? (-1 for all of them)
-limit_products = 10
+limit_products = 5
 # base currency (where the funds are taken from)
 base_currency = "EUR"
 # recurring buy amount
@@ -27,40 +19,11 @@ buy_amount = 50
 max_buy_products = 5
 simulation = True
 
-class Header:
-    """Display header with clock."""
-
-    def __rich__(self) -> Panel:
-        grid = Table.grid(expand=True)
-        grid.add_column(justify="center", ratio=1)
-        grid.add_column(justify="right")
-        grid.add_row(
-            "[b]Trading[/b] Engine",
-            datetime.now().ctime().replace(":", "[blink]:[/]"),
-        )
-        return Panel(grid, style="white on blue")
-
-def make_layout() -> Layout:
-    """Define the layout."""
-    layout = Layout(name="root")
-
-    layout.split(
-        Layout(name="header", size=3),
-        Layout(name="main", ratio=1),
-        Layout(name="footer", size=7),
-    )
-    layout["main"].split(
-        Layout(name="side"),
-        Layout(name="body", ratio=2, minimum_size=60),
-        direction="horizontal",
-    )
-    layout["side"].split(Layout(name="box1"), Layout(name="box2"))
-    return layout
-
 with open('config.json') as config_file:
     data = json.load(config_file)
 
-trading = TradingEngine(data['key'], data['b64secret'], data['passphrase'], base_currency, max_buy_products, buy_amount, Strategy.TopGainers)
+trading = TradingEngine(data['key'], data['b64secret'], data['passphrase'],
+                        base_currency, max_buy_products, buy_amount, Strategy.TopLosers, limit_products)
 
 coinbase_account = trading.get_account()
 
@@ -76,7 +39,7 @@ print(
 # print(tradable_products)
 
 if simulation:
-    trading.simulate_period(trading_interval_days, 30)
+    trading.simulate_period(trading_interval_days, trading_periods)
 else:
     # FUCKED UP
     sorted_market_trend = trading.get_last_market_trends(
@@ -89,5 +52,6 @@ else:
     for pid in ordering_products:
         print(f"{pid}: {ordering_products[pid]:.2f}%")
 
-    orders = trading.get_buy_quotes(buy_amount, ordering_products, tradable_products)
+    orders = trading.get_buy_quotes(
+        buy_amount, ordering_products, tradable_products)
     trading.execute_orders(orders)
--- trading.py
+++ trading.py
@@ -5,21 +5,13 @@ import json
 import os
 from datetime import datetime, timedelta
 import logging
-from rich import print
-from rich.logging import RichHandler
-
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
-
-log = logging.getLogger("trading")
 
 from portfolio import Order, Portfolio
 
 class Strategy(enum.Enum):
    TopGainers = 1
    TopLosers = 2
+   Mixed = 3
 
 class PriceTrackerWsClient(cbpro.WebsocketClient):
     def start(self, url, products):
@@ -27,16 +19,16 @@ class PriceTrackerWsClient(cbpro.WebsocketClient):
         self.products = list(products)
         self.channels = ['ticker']
         self.prices = {}
-        log.info(f"Subscribing to {len(self.products)} products tickers")
+        print(f"Subscribing to {len(self.products)} products tickers")
         super().start()
 
     def on_message(self, msg):
         if "price" in msg:
             self.prices[msg['product_id']] = 1/float(msg['price'])
-            #log.info(f"{msg['product_id']}: {self.prices[msg['product_id']]:.5f}")
+            #print(f"{msg['product_id']}: {self.prices[msg['product_id']]:.5f}")
 
     def on_close(self):
-        log.info("-- Goodbye! --")
+        print("-- Goodbye! --")
 
     def get_price(self, product_id):
         if product_id in self.prices:
@@ -47,7 +39,7 @@ class PriceTrackerWsClient(cbpro.WebsocketClient):
 
 class TradingEngine:
     def __init__(self, key, secret, passphrase, base_currency, max_buy_products, 
-    buy_amount, strategy):
+    buy_amount, strategy, limit_products):
         self.public_client = cbpro.PublicClient()
         self.auth_client = cbpro.AuthenticatedClient(key, secret, passphrase,
                                                 api_url="https://api-public.sandbox.pro.coinbase.com")
@@ -55,6 +47,10 @@ class TradingEngine:
         self.max_buy_products = max_buy_products
         self.buy_amount = buy_amount
         self.strategy = strategy
+        self.portfolio = Portfolio(base_currency)
+        self.tickers_cache = {}
+        self.last_strategy_flag = True
+        self.limit_products = limit_products
 
     def get_account(self):
         coinbase_accounts = self.auth_client.get_coinbase_accounts()
@@ -73,7 +69,7 @@ class TradingEngine:
                 tradable_products[product['id']] = product
         return tradable_products
 
-    def get_last_market_trends(self, tradable_products, start, end, limit_products = -1):
+    def get_last_market_trends(self, tradable_products, start, end):
         market_trend = {}
         for _, pid in enumerate(tradable_products):
             p = tradable_products[pid]            
@@ -81,7 +77,7 @@ class TradingEngine:
             ets = str(end.timestamp())
             
             if sts not in self.tickers_cache[pid] or ets not in self.tickers_cache[pid]:
-                log.warn(f"Unable to compute trends for {pid}, missing ticker informations")
+                print(f"Unable to compute trends for {pid}, missing ticker informations")
                 continue
 
             now = self.tickers_cache[pid][ets]
@@ -89,10 +85,18 @@ class TradingEngine:
             gain = (now["close"]-old["close"])/now["close"] * 100.0
             market_trend[p['id']] = gain
 
+        if self.strategy == Strategy.TopGainers:
+            reverse = True
+        elif self.strategy == Strategy.TopLosers:
+            reverse = False
+        else:
+            self.last_strategy_flag = not self.last_strategy_flag
+            reverse = self.last_strategy_flag
+
         # sort by gain
-        sorted_list = sorted(market_trend.items(), key=lambda item: item[1], reverse=self.strategy == Strategy.TopGainers)
-        if limit_products > 0 and len(market_trend) >= limit_products:
-            sorted_list = sorted_list[:limit_products]
+        sorted_list = sorted(market_trend.items(), key=lambda item: item[1], reverse=reverse)
+        if self.limit_products > 0 and len(market_trend) >= self.limit_products:
+            sorted_list = sorted_list[:self.limit_products]
         sorted_market_trend = dict(sorted_list)
         return sorted_market_trend
     
@@ -105,7 +109,7 @@ class TradingEngine:
             if len(top) == 0 or val > top[1]:
                 top = (p, val)
             orders[p] = val
-            #log.info(f"orders[{p}]: {orders[p]}")
+            #print(f"orders[{p}]: {orders[p]}")
 
         while True:
             untouched = True
@@ -113,7 +117,7 @@ class TradingEngine:
                 min_value = float(tradable_products[p]['min_market_funds'])
                 if orders[p] < min_value:
                     orders[top[0]] += orders[p]
-                    log.info(f"{p} too small ({orders[p]:.2f} < {min_value:.2f}), adding to top product {top[0]} = {orders[top[0]]:.2f}")
+                    print(f"{p} too small ({orders[p]:.2f} < {min_value:.2f}), adding to top product {top[0]} = {orders[top[0]]:.2f}")
                     orders.pop(p)
                     untouched = False
             if untouched: break
@@ -122,50 +126,59 @@ class TradingEngine:
     def execute_orders(self, orders):
         exec_orders = []
         for o in orders:
-            log.info(f"Executing order for {o}: {orders[o]} {self.base_currency}")
+            print(f"Executing order for {o}: {orders[o]} {self.base_currency}")
             order = self.auth_client.place_market_order(
                 product_id=o, side='buy', funds=orders[o])
-            log.info(order)
+            print(order)
             exec_orders.append(order)
         return exec_orders
 
+    def ticks_contains_date(self, tickers, expected_date):
+        for t in tickers:
+            dt = datetime.fromtimestamp(int(t[0]))
+            dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+            if dt == expected_date:
+                return True
+        return False
+        
     def prepare_data(self, products, begin, end):
-        self.tickers_cache = {}
         cache_file = "cache.json"
         if os.path.exists(cache_file) and os.path.getsize(cache_file) > 0:
-            log.info(f"Reading cache from file")
+            print(f"Reading cache from file")
             with open(cache_file, "r") as f:
                 self.tickers_cache = json.loads(f.read())
 
         for p in products:
-            log.info(f"Lookup {p} historical data {begin.isoformat()}-{end.isoformat()}")
+            print(f"Lookup {p} historical data {begin.isoformat()}-{end.isoformat()}")
             begin = begin.replace(hour=0, minute=0, second=0, microsecond=0)
             end = end.replace(hour=0, minute=0, second=0, microsecond=0)
 
             if p not in self.tickers_cache:
-                #log.info(f"Product {p} not in self.tickers_cache!")
+                #print(f"Product {p} not in self.tickers_cache!")
                 self.tickers_cache[p] = {}
             else:
                 begin_ts = str(begin.timestamp()) 
                 end_ts = str(end.timestamp())
                 if begin_ts in self.tickers_cache[p] and end_ts in self.tickers_cache[p]:
-                    log.info(f"Product {p} already in cache!")
+                    print(f"Product {p} already in cache!")
                     continue
                 else:
                     pass
-                    #log.info(f"Missing timestamps for {p} {begin_ts} - {end_ts}")
-                    #log.info(cache[p])
+                    #print(f"Missing timestamps for {p} {begin_ts} - {end_ts}")
+                    #print(cache[p])
                 
             
             tickers = self.public_client.get_product_historic_rates(
                     p, start=begin, end=end, granularity=86400)
 
             diff = end.date()-begin.date()
-            if len(tickers) < diff.days:
-                log.info(f"Missing ticks {len(tickers)} instead of {diff.days}")
+
+            if not self.ticks_contains_date(tickers, begin) or not self.ticks_contains_date(tickers, end):
+                print(f"Missing required ticks!")
                 continue
+
             for t in tickers:
-                #log.info(t)
+                #print(t)
                 dt = datetime.fromtimestamp(int(t[0]))
                 dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
                 timestamp = str(dt.timestamp())
@@ -179,10 +192,9 @@ class TradingEngine:
 
         with open(cache_file, "w") as f:
             f.write(json.dumps(self.tickers_cache))
-        #log.info(cache)
+        #print(cache)
 
     def simulate_period(self, trading_interval_days: int, periods: int):
-        portfolio = Portfolio(self.base_currency)
         begin = datetime.today() - timedelta(days=(periods*trading_interval_days))
 
         self.trading_interval_days = trading_interval_days
@@ -199,7 +211,7 @@ class TradingEngine:
             end = end.replace(hour=0, minute=0, second=0, microsecond=0)
             print(f"Computing period {start.isoformat()} - {end.isoformat()}")
 
-            trends = self.get_last_market_trends(tradable_products, start, end, limit_products=5)
+            trends = self.get_last_market_trends(tradable_products, start, end)
 
             ordering_products = self.get_buy_quotes(trends, tradable_products)            
             #print(ordering_products)
@@ -209,9 +221,9 @@ class TradingEngine:
                 # ticker information contains value for a unit of cryptocurrency
                 unit_value = 1.0/self.tickers_cache[pid][str(end.timestamp())]["close"]
                 order.buy(end, ordering_products[pid], unit_value)
-                portfolio.add(order)
+                self.portfolio.add(order)
 
-        print(portfolio.summary(self.tickers_cache))
+        print(self.portfolio.summary(self.tickers_cache))
         print(f"Strategy used: {self.strategy.name} across last {periods} periods of {trading_interval_days} days each")
 
     def execute(self, order):
